0ff7b775acf8166bb20b5789bda1b79f83926864
Dmytro Tolok <dmytro.tolok@stfalcon.com>
2019-08-12 16:18:14 +0300

    chore(eslint): extends eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,37 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
+    '@vue/standard',
+    'plugin:vue/base',
     'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', { props: false }],
+    camelcase: ['error', { properties: 'never', ignoreDestructuring: true }],
+    'import/extensions': ['js', 'vue', 'json'],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: { multiline: true, consistent: true },
+        ObjectPattern: { multiline: true, consistent: true },
+        ImportDeclaration: { multiline: true, consistent: true },
+        ExportDeclaration: { multiline: true, consistent: true },
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'array-bracket-spacing': ['error', 'never'],
+    eqeqeq: ['error', 'always'],
+    semi: ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+  },
+};

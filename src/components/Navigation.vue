e00b2369ce8679e4f76e74a46a8fa07598853fe0
Dmytro Tolok <dmytro.tolok@stfalcon.com>
2019-08-14 09:45:43 +0300

    refactor(app): removes redundant components, sets basic structure

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navigation">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  width: 100%;
+}
+</style>

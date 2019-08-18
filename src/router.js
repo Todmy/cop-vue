6bbc421b2e3416c06234e41675e4131e91d3df22
Dmytro Tolok <dmytro.tolok@stfalcon.com>
2019-08-18 15:52:18 +0300

    fix(router): fixes router mode

--- src/router.js
+++ src/router.js
@@ -8,17 +8,13 @@ function loadView(view) {
 }
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
       component: loadView('Home'),
-    },
-    {
-      path: '/singleton',
-      name: 'singleton',
-      component: loadView('Singleton'),
-      meta: { rootHolder: true },
     },
   ],
 });
